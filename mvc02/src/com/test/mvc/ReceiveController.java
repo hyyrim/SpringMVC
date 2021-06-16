d902fa3f42c461b665187b6683f9a10e26b5c010
hyyrim <realD1006@gmail.com>
2021-06-16 14:16:22 +0900

    SpringMVC/DB연동

---
+++ mvc02/src/com/test/mvc/ReceiveController.java
@@ -0,0 +1,38 @@
+/*==================================
+	ReceiveController.java
+	- 사용자 정의 컨트롤러 클래스
+==================================*/
+
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// ※ Spring 의 『Controller』 인터페이스를 구현하는 방법을 통해
+//	  사용자 정의 컨트롤러 클래스를 구현한다.	  
+public class ReceiveController implements Controller
+{
+		
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		// 액션 코드 
+
+		ModelAndView mav = new ModelAndView();
+		
+		// 한글 깨짐 방지
+		request.setCharacterEncoding("UTF-8");
+		String userName = request.getParameter("userName");
+		
+		mav.addObject("userName", userName);
+		mav.setViewName("/WEB-INF/views/Receive.jsp");
+		
+		return mav;
+		
+	}
+	
+}
